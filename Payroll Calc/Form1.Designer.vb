a18b10fec8e1a6f98b1054a3662d41095265ffe9
Carson Parker <carsonparker0823@gmail.com>
2025-03-04 13:25:45 -0500

    Implement everything

--- Payroll Calc/Form1.Designer.vb
+++ Payroll Calc/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmPayrollCalculator
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -25,6 +25,19 @@ Partial Class Form1
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtGrossPay = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblNetpay = New System.Windows.Forms.Label()
+        Me.lblFica = New System.Windows.Forms.Label()
+        Me.lblFederal = New System.Windows.Forms.Label()
+        Me.lblState = New System.Windows.Forms.Label()
+        Me.lblNet = New System.Windows.Forms.Label()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -33,7 +46,7 @@ Partial Class Form1
         Me.PictureBox1.Image = Global.Payroll_Calc.My.Resources.Resources.payroll
         Me.PictureBox1.Location = New System.Drawing.Point(-5, -2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(397, 259)
+        Me.PictureBox1.Size = New System.Drawing.Size(323, 211)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
@@ -58,15 +71,158 @@ Partial Class Form1
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Paycheck " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Calculation"
         '
-        'Form1
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Leelawadee UI", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(226, 233)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(229, 38)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Enter Gross Pay:"
+        '
+        'txtGrossPay
+        '
+        Me.txtGrossPay.Location = New System.Drawing.Point(461, 232)
+        Me.txtGrossPay.Multiline = True
+        Me.txtGrossPay.Name = "txtGrossPay"
+        Me.txtGrossPay.Size = New System.Drawing.Size(122, 39)
+        Me.txtGrossPay.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(139, 342)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 18)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "FICA:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(313, 342)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(101, 18)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Federal Tax:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(514, 342)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(84, 18)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "State Tax:"
+        '
+        'lblNetpay
+        '
+        Me.lblNetpay.AutoSize = True
+        Me.lblNetpay.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNetpay.Location = New System.Drawing.Point(173, 394)
+        Me.lblNetpay.Name = "lblNetpay"
+        Me.lblNetpay.Size = New System.Drawing.Size(202, 22)
+        Me.lblNetpay.TabIndex = 8
+        Me.lblNetpay.Text = "Net Paycheck Income"
+        '
+        'lblFica
+        '
+        Me.lblFica.AutoSize = True
+        Me.lblFica.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFica.Location = New System.Drawing.Point(208, 344)
+        Me.lblFica.Name = "lblFica"
+        Me.lblFica.Size = New System.Drawing.Size(0, 18)
+        Me.lblFica.TabIndex = 9
+        '
+        'lblFederal
+        '
+        Me.lblFederal.AutoSize = True
+        Me.lblFederal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFederal.Location = New System.Drawing.Point(430, 342)
+        Me.lblFederal.Name = "lblFederal"
+        Me.lblFederal.Size = New System.Drawing.Size(0, 18)
+        Me.lblFederal.TabIndex = 10
+        '
+        'lblState
+        '
+        Me.lblState.AutoSize = True
+        Me.lblState.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblState.Location = New System.Drawing.Point(615, 344)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(0, 18)
+        Me.lblState.TabIndex = 11
+        '
+        'lblNet
+        '
+        Me.lblNet.AutoSize = True
+        Me.lblNet.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNet.Location = New System.Drawing.Point(410, 394)
+        Me.lblNet.Name = "lblNet"
+        Me.lblNet.Size = New System.Drawing.Size(0, 18)
+        Me.lblNet.TabIndex = 12
+        '
+        'btnCompute
+        '
+        Me.btnCompute.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnCompute.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCompute.ForeColor = System.Drawing.Color.Snow
+        Me.btnCompute.Location = New System.Drawing.Point(49, 287)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(159, 42)
+        Me.btnCompute.TabIndex = 13
+        Me.btnCompute.Text = "Compute Taxes"
+        Me.btnCompute.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.Snow
+        Me.btnClear.Location = New System.Drawing.Point(271, 287)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(159, 42)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(473, 287)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(159, 42)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frmPayrollCalculator
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.lblNet)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.lblFederal)
+        Me.Controls.Add(Me.lblFica)
+        Me.Controls.Add(Me.lblNetpay)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtGrossPay)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Name = "Form1"
+        Me.Name = "frmPayrollCalculator"
         Me.Text = "Payroll Calculator"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -77,4 +233,17 @@ Partial Class Form1
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtGrossPay As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblNetpay As Label
+    Friend WithEvents lblFica As Label
+    Friend WithEvents lblFederal As Label
+    Friend WithEvents lblState As Label
+    Friend WithEvents lblNet As Label
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
 End Class
--- Payroll Calc/Form1.vb
+++ Payroll Calc/Form1.vb
@@ -1,25 +1,61 @@
-﻿Public Class Form1
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+﻿Public Class frmPayrollCalculator
+    ' Constants for tax rates
+    Private Const cdecFica As Decimal = 0.0765D
+    Private Const cdecFed As Decimal = 0.22D
+    Private Const cdecState As Decimal = 0.04D
 
+    Private Sub frmPayrollCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Clear all labels and set focus to Gross Pay input
+        ClearFields()
     End Sub
 
-    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs)
+    ' Event handler for Compute Taxes button
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim strIncome As String = txtGrossPay.Text
+        Dim decIncome As Decimal
 
+        ' Input validation
+        If Not Decimal.TryParse(strIncome, decIncome) OrElse decIncome < 0 Then
+            MessageBox.Show("Please enter a valid positive number for Gross Pay.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtGrossPay.Clear()
+            txtGrossPay.Focus()
+            Exit Sub
+        End If
+
+        ' Tax calculations
+        Dim decFica As Decimal = decIncome * cdecFica
+        Dim decFederal As Decimal = decIncome * cdecFed
+        Dim decState As Decimal = decIncome * cdecState
+        Dim decNet As Decimal = decIncome - (decFica + decFederal + decState)
+
+        ' Display results formatted as currency
+        lblFica.Text = decFica.ToString("C2")
+        lblFederal.Text = decFederal.ToString("C2")
+        lblState.Text = decState.ToString("C2")
+        lblNetPay.Text = decNet.ToString("C2")
     End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs)
-
+    ' Event handler for Clear button
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearFields()
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
-
+    ' Event handler for Exit button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
     End Sub
 
-    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
-
+    ' Method to clear all input/output fields
+    Private Sub ClearFields()
+        txtGrossPay.Clear()
+        lblFica.Text = ""
+        lblFederal.Text = ""
+        lblState.Text = ""
+        lblNetPay.Text = ""
+        txtGrossPay.Focus()
     End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles lblNetpay.Click
 
     End Sub
 End Class
